66ac39912be6e44316646978a2e517846064f612
Bahar <bhrakay235@gmail.com>
2022-01-31 14:50:25 +0300

    new

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,8 +8,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:ttgame/main.dart';
-import 'package:ttgame/tt_game.dart';
-
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
